dbde31aea85b855957c8a06f897ea1b1a40c54ef
Oleg Mysin <7nacu7@gmail.com>
2024-07-18 12:35:10 +0300

    step2 add battery level (wasm incorrect)

--- composeApp/src/wasmJsMain/kotlin/main.kt
+++ composeApp/src/wasmJsMain/kotlin/main.kt
@@ -5,6 +5,8 @@ import kotlinx.browser.document
 @OptIn(ExperimentalComposeUiApi::class)
 fun main() {
     ComposeViewport(document.body!!) {
-        App()
+        App(
+            batteryManager = BatteryManager()
+        )
     }
 }
