890f84f0d502974b71aceaecdeb7b30949e94d47
Oleg Mysin <7nacu7@gmail.com>
2024-07-18 15:06:11 +0300

    step3 add res (image + strings)

--- composeApp/src/commonMain/kotlin/App.kt
+++ composeApp/src/commonMain/kotlin/App.kt
@@ -1,11 +1,19 @@
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.size
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import kmpprojectlack.composeapp.generated.resources.Res
+import kmpprojectlack.composeapp.generated.resources.hello_world
+import kmpprojectlack.composeapp.generated.resources.pikachu
+import org.jetbrains.compose.resources.painterResource
+import org.jetbrains.compose.resources.stringResource
 import org.jetbrains.compose.ui.tooling.preview.Preview
 
 
@@ -21,7 +29,12 @@ fun App(
             contentAlignment = Alignment.Center
         ) {
             Column {
-                Text(text = "Hello world")
+                Image(
+                    modifier = Modifier.size(240.dp),
+                    painter = painterResource(Res.drawable.pikachu),
+                    contentDescription = "pikachu"
+                )
+                Text(text = stringResource(Res.string.hello_world))
                 Text(text = Greeting().greet())
                 Text(text = "Уровень заряда батареи: ${batteryManager.getBatteryLevel()}")
             }
